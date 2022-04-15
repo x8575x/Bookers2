09ba092d2f301c392f80386679f653ee94fb64c3
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-15 10:41:39 +0000

    [Add]ヘッダーのレイアウト完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "homes#top"
+  get "home/about" => "homes#about"
 
   resources :books, only: [:index, :show]
   resources :users, only: [:index, :show, :edit]
