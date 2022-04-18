eedbc4a588ef45b9eb0c591fec93ab42f3d3b596
EC2 Default User <ec2-user@ip-172-31-22-189.ec2.internal>
2022-04-18 04:56:18 +0000

    [Add]投稿編集ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about" => "homes#about"
 
-  resources :books, only: [:create, :index, :show, :edit]
+  resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit]
 
 
